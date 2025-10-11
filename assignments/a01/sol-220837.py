fe4b2c674ab155c2b22df38ab438fa74b8dc0173
vzamorag94-ux <vzamorag94@gmail.com>
2025-10-10 23:41:40 -0600

    Create a1-220837.py

---
+++ assignments/a01/sol-220837.py
@@ -0,0 +1,11 @@
+# Make sure you are on 'develop' first
+git checkout develop
+
+# Create and switch to your assignment branch
+STUDENT_ID=130524        # <-- change to your ID
+git checkout -b assignment/a01-$STUDENT_ID
+
+# Confirm you’re on it:
+git status
+# Expect: On branch assignment/a01-220837
+
