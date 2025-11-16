6f00e18fe9770bcd4cb43ed92fca664e700583ba
Arturo Soberon <47676848+ArturoSbr@users.noreply.github.com>
2025-11-16 08:45:31 -0600

    WIP skeleton code with all docstrings

--- assignments/a03/code/hints.py
+++ assignments/a03/code/hints.py
@@ -27,7 +27,7 @@ def init_theta(p, random_state):
 
     Returns
     -------
-    ndarray of shape (p,).
+    Numpy ndarray of shape (p,).
     """
     pass
 
@@ -55,6 +55,10 @@ def sigma(X, theta):
 # Q5. Vectorized function that classifies observations based on their probability
 def classify(X, theta, threshold=0.5):
     """Classifies all observations in the data based on their predicted probabilities.
+
+    This function calls `sigma(X, theta)` to calculate the array of predicted
+    probabilities and then classifies each observation using `threshold`. If a given
+    probability is greater than `threshold`, it labels it as `1` and `0` otherwise.
     
     Parameters
     ----------
@@ -67,11 +71,15 @@ def classify(X, theta, threshold=0.5):
         A float between 0.0 and 1.0. Observations whose predicted probabilities are
         strictly greater than this threshold are classified as 1 and 0 otherwise.
         Defaults to 0.5.
+
+    Returns
+    -------
+    Numpy ndarray of shape (n,) representing the predicted labels for each observation.
     """
     pass
 
 
-# Evaluate loss
+# Q6. Evaluate current loss given theta
 def loss(X, y, theta):
     """Vectorized calculation of the negative log-likelihood given theta.
     
@@ -84,12 +92,32 @@ def loss(X, y, theta):
         Target of shape (n,) cointaining the observed labels of each observation.
     theta:
         The parameters of a logistic regression model. Its shape is (p,).
+
+    Returns
+    -------
+    Float representing the loss value given the current estimates of theta.
     """
     pass
 
 
-# Evaluate gradient
+# Q7. Evaluate gradient
 def gradient(X, y, theta):
+    """Vectorized calculation of the gradient of the loss function at theta.
+    
+    Parameters
+    ----------
+    X: numpy.ndarray
+        Feature matrix of shape (n, p) containing the observed characteristics of each
+        observation. Its first column is always full of ones.
+    y: numpy.ndarray
+        Target of shape (n,) cointaining the observed labels of each observation.
+    theta:
+        The parameters of a logistic regression model. Its shape is (p,).
+
+    Returns
+    -------
+    Numpy ndarray of shape (p,) representing the gradient of the loss function at theta.
+    """
     pass
 
 
@@ -97,4 +125,48 @@ def gradient(X, y, theta):
 def gradient_descent(
     X, y, random_state, max_iter=100000, min_gain=1e-6, step_size=0.001
 ):
-    pass
+    """Naive gradient descent algorithm for logistic regression.
+    
+    This function is an implementation of the original gradient descent algorithm for
+    logistic regression. It minimizes the negative loss-likelihood cross entropy loss
+    by iteratively updating theta using the loss's gradient.
+    
+    Parameters
+    ----------
+    X: numpy.ndarray
+        Feature matrix of shape (n, p) containing the observed characteristics of each
+        observation. Its first column is always full of ones.
+    y: numpy.ndarray
+        Target of shape (n,) cointaining the observed labels of each observation.
+    random_state: int
+        The seed used to initialize theta.
+    max_iter: int
+        The maximum number of updates to theta. Defaults to 100000.
+    min_gain: float
+        The minimum gain in the loss function to trigger another iteration. If the
+        updated parameters, theta, fail to cause a gain larger than this threshold, the
+        algorithm comes to an early stop. Defaults to 1e-6
+    step_size: Float
+        This factor represents the fraction of the gradient used to update theta at each
+        iteration (`theta_1 = theta_0 - step_size * nabla`).
+
+    Returns
+    -------
+    Numpy ndarray of shape (p,) representing the final parameters estimated by the
+    naive gradient descent algorithm.
+    """
+    # Init theta randomly
+    theta_0 = init_theta(p=X.shape[1], random_state=random_state)
+
+    # Iterate from 1 to T
+    for i in range(max_iter):
+
+        # Use previous function to calculate necessary inputs
+        loss_0 = True
+        nabla = True
+        theta_1 = theta_0 - step_size * nabla
+        loss_1 = True
+        gain = abs(loss_1 - loss_0)  # Compare with min_gain to check for early stop
+    
+    # Return final theta (in case of no early stop)
+    return theta_1
