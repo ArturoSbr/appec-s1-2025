da422abd210c5fcdb890852b858112d40d602a51
unknown <karenpmundo>
2025-12-08 16:40:51 -0600

    tarea2-kpmr-v6

--- assignments/a02/code/sol-225824.py
+++ assignments/a02/code/sol-225824.py
@@ -21,7 +21,7 @@ table_reviews['review_answer_timestamp'] = pd.to_datetime(
 )
 table_reviews.info()
 
-#dejar solante la reseña más reciente por cada orden
+# dejar solante la reseña más reciente por cada orden
 table_reviews = (
     table_reviews
     .sort_values(['order_id', 'review_answer_timestamp'])
@@ -46,16 +46,16 @@ agg_items.describe()
 # Q4. Join table_reviews and agg_items to create df
 
 df = pd.merge(
-    left= table_reviews,
-    right= agg_items,
-    how= 'inner',
-    on= ['order_id']
+    left=table_reviews,
+    right=agg_items,
+    how='inner',
+    on=['order_id']
 )
 df.info()
 
 # Q5. Calculate days_delay in table_orders
 
-#convertir a formato de fecha
+# convertir a formato de fecha
 table_orders['order_estimated_delivery_date'] = pd.to_datetime(
     table_orders['order_estimated_delivery_date'],
     format = '%Y-%m-%d %H:%M:%S'
@@ -67,9 +67,7 @@ table_orders['order_delivered_customer_date'] = pd.to_datetime(
 )
 table_orders.info()
 
-#crear var days_delay
-#96,476
-#means days_delay -11.877
+# crear var days_delay
 
 table_orders['days_delay'] = (
     table_orders['order_delivered_customer_date']
@@ -79,24 +77,25 @@ table_orders['days_delay'] = (
 table_orders.describe()
 
 # Q6. Join df and table_orders to add days_delay
+
 df = pd.merge(
-    left= df,
-    right= table_orders[['order_id', 'order_status', 'days_delay']],
-    how= 'inner',
-    on= ['order_id']
+    left=df,
+    right=table_orders[['order_id', 'order_status', 'days_delay']],
+    how='inner',
+    on=['order_id']
 )
 df['days_delay'].isna().sum()
 
 # Q7. Join table_items and table_products to calculate avg_pics
 
-median_photos = table_products['product_photos_qty'].median()
+median_photos =table_products['product_photos_qty'].median()
 table_products['product_photos_qty'] = \
 table_products['product_photos_qty'].fillna(median_photos)
 
 table_products['product_photos_qty'].describe()
 
 # Unir product_id' y 'product_photos_qty' a table_items
-#98,666
+
 tmp_items = table_items.merge(
     table_products[['product_id', 'product_photos_qty']],
     on='product_id',
@@ -104,7 +103,7 @@ tmp_items = table_items.merge(
 )
 
 # Añadir avg_pics por cada orden
-#2.232
+
 agg_pics = (
     tmp_items
     .groupby('order_id', as_index=False)
@@ -116,9 +115,9 @@ agg_pics.describe()
 # Q8. Add avg_pics to df
 df = pd.merge(
     left= df,
-    right= agg_pics[['order_id', 'avg_pics']],
-    how= 'inner',
-    on= ['order_id']
+    right=agg_pics[['order_id', 'avg_pics']],
+    how='inner',
+    on=['order_id']
 )
 df.describe()
 
@@ -127,44 +126,31 @@ df['const'] = 1
 df.describe()
 
 # Q10. Fit model 1
-df = df[df['order_status'] == 'delivered'].copy()
-df.describe()
+df_model = df[df['order_status'] == 'delivered'].copy()
+df_model.describe()
 
-#selección de variables
+# seleccion de variables
 cols = ['happy', 'const', 'n_items', 'avg_price', 'avg_shipping',
         'days_delay', 'avg_pics']
-df_model = df[cols]
+X = df_model[['const', 'n_items', 'avg_price', 'avg_shipping',
+              'days_delay', 'avg_pics']]
+y = df_model['happy']
 
-#modelo
-# Convert days_delay from Timedelta to numeric days
+# modelo
+# Convert days_delay 
 
-m1 = sm.Logit(
-    df_model['happy'],
-    df_model[['const', 'n_items', 'avg_price', 'avg_shipping',
-              'days_delay', 'avg_pics']],
-    missing='drop'
-)
+m1 = sm.Logit(y, X, missing='drop')
 
 m1_res = m1.fit()
 m1_res.summary()
 
-#m1_res.nobs == 95824
-#params['const']      ≈ 1.314973
-#params['n_items']    ≈ -0.498888
-#params['days_delay'] ≈ -0.063472
-
 # Q11. Fit model 2
 
-df_m2 = df[(df['order_status'] == 'delivered') & (df['n_items'] == 1)].copy()
+df_m2=df[(df['order_status'] == 'delivered') & (df['n_items'] == 1)].copy()
 
+X2=df_m2[['const', 'avg_price', 'avg_shipping', 'days_delay', 'avg_pics']]
+y2=df_m2['happy']
 
-#elección de variables
-m2 = sm.Logit(
-    df_m2['happy'],
-    df_m2[['const', 'avg_price', 'avg_shipping',
-              'days_delay', 'avg_pics']],
-    missing='drop'
-)
-
-m2_res = m2.fit()
+m2=sm.Logit(y2, X2, missing='drop')
+m2_res=m2.fit()
 m2_res.summary()
