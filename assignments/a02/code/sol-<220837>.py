d781991835a7e4acc7f98cd3669e06995e01dcb8
vzamorag94-ux <vzamorag94@gmail.com>
2025-12-07 22:56:35 -0600

    Update sol-<220837>.py

--- assignments/a02/code/sol-<220837>.py
+++ assignments/a02/code/sol-<220837>.py
@@ -1,15 +1,23 @@
+"""This file will help you get started with Assignment 2.
+
+You don't need any other dependencies, and you also don't need to read any other
+tables. Just remember to go to your own branch, copy this file, rename it
+`sol-<student ID>.py`, modify it accordingly and then push it (again, to YOUR
+branch). Your results will be displayed in the repo's "Actions" tab.
+
+Also, remember to lint your code like I taught you.
+"""
+
 # Imports
 import os
 import pandas as pd
 import statsmodels.api as sm
 
-# Read tables (updated paths)
-DATA_DIR = os.path.join("assignments", "a02", "data")
-
-table_items = pd.read_csv(os.path.join(DATA_DIR, "items.csv"))
-table_orders = pd.read_csv(os.path.join(DATA_DIR, "orders.csv"))
-table_products = pd.read_csv(os.path.join(DATA_DIR, "products.csv"))
-table_reviews = pd.read_csv(os.path.join(DATA_DIR, "reviews.csv"))
+# Read tables (DON'T CHANGE THIS)
+table_items = pd.read_csv(os.path.join("..", "data", "items.csv"))
+table_orders = pd.read_csv(os.path.join("..", "data", "orders.csv"))
+table_products = pd.read_csv(os.path.join("..", "data", "products.csv"))
+table_reviews = pd.read_csv(os.path.join("..", "data", "reviews.csv"))
 
 # Q1. Create binary target
 table_reviews["happy"] = (table_reviews["review_score"] >= 4).astype(int)
